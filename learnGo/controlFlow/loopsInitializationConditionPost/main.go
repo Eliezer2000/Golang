b668c04841d2fa5cc825521548d28a0898d4d146
Eliezer2000 <eliezeralvesbernardino@gmail.com>
2023-12-24 22:20:20 -0300

    feat: control flow: loops initialization condition post

---
+++ learnGo/controlFlow/loopsInitializationConditionPost/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+	- For
+    - Inicialização, condição, pós
+    - Ponto e vírgula?
+    - gobyexample.com
+    - Não existe while!
+*/
+
+
+func main() {
+
+	for x := 0; x < 10; x++ {
+		fmt.Println(x)
+	}
+}
