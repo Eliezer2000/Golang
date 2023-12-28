2421e9877db364497d90e21e85f4a44a5ab52e01
Eliezer2000 <eliezeralvesbernardino@gmail.com>
2023-12-27 22:38:33 -0300

    feat: data grouping, and Slice: multi-dimensional

---
+++ learnGo/dataGrouping/sliceMulti-dimensional/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+	- Slices multi-dimensionais são slices que contem slices.
+	- São como planilhas.
+	- [][]type
+*/
+
+func main() {
+
+	ss := [][]int{
+		[]int{1, 2, 3, 4, 5, 6},
+		[]int{7, 8, 9, 10, 11, 12},
+		[]int{13, 14, 15, 16, 17, 18},
+	}
+	fmt.Println(ss[2][4])
+}
