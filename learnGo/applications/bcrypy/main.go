279ab5bd4d08fcbac49d6f1d8847892fd2f0f0e0
Eliezer2000 <eliezeralvesbernardino@gmail.com>
2024-01-07 22:31:58 -0300

    feat: learnGo exercises: nivel #8 - 1

---
+++ learnGo/applications/bcrypy/main.go
@@ -0,0 +1,14 @@
+package main
+
+
+
+
+func main(){
+//	senha := "29fevereiro2024"
+	
+//	sb, err := bcrypt.GenerateFromPassword([]byte(senha), 10)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	fmt.Println(sb)
+}
