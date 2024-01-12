c1e35d5a7a1e7c6bc815fceb557f71d71a685928
Eliezer2000 <eliezeralvesbernardino@gmail.com>
2024-01-11 23:31:34 -0300

    feat: learnGo exercises: nivel #9 - 1

---
+++ learnGo/exercises/nivel#9/1/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+	- Alem da goroutine principal, crie duas outras goroutines.
+	- Cada goroutine adicional devem fazer um print separado.
+	- Utilize waitgroups para fazer com que suas goroutines finalizem antes de o programa terminar.
+*/
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	novasgoroutines(100)
+	wg.Wait()
+}
+
+func novasgoroutines(i int) {
+	wg.Add(i)
+	for j := 0; j < i; j++ {
+		x := j
+		go func(i int) {
+			fmt.Println("Eu sou a goroutine número:", i+1)
+			wg.Done()
+		}(x)
+	}
+}
