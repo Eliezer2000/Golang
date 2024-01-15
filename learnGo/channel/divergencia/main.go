40ce6da3b6f39efc6853d42e71646c711094019b
Eliezer2000 <eliezeralvesbernardino@gmail.com>
2024-01-14 22:38:25 -0300

    feat: learnGo Channels: Divergencia

---
+++ learnGo/channel/divergencia/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	canal1 := make(chan int)
+	canal2 := make(chan int)
+	funcoes := 5
+
+	go manda(100, canal1)
+	go outra(funcoes, canal1, canal2)
+
+	for v := range canal2 {
+		fmt.Println(v)
+	}
+}
+
+func manda(n int, canal chan int) {
+	for i := 0; i < n; i++ {
+		canal <- i
+	}
+	close(canal)
+}
+
+func outra(funcoes int, canal1, canal2 chan int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < funcoes; i++ {
+		wg.Add(1)
+		go func() {
+			for v := range canal1{
+				canal2 <- trabalho(v)
+				wg.Done()
+			}
+		}()
+	}
+	wg.Wait()
+	close(canal2)
+
+	for v := range canal1 {
+		
+		go func(x int) {
+			canal2 <- trabalho(x)
+			
+		}(v)
+	}
+}
+
+func trabalho(n int) int {
+	time.Sleep(time.Millisecond * 1000)
+	return n
+}
