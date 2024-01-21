a742ae4eae2aac179d1a7ff128ce1e32a3c502a2
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2024-01-20 22:42:07 -0300

    feat: learnGo exercises: nivel #1

---
+++ learnGo/exercises/nivel1/nivel1_5/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/*
+- Utilizando a solução do exercício anterior:
+    1. Em package-level scope, utilizando a palavra-chave var, crie uma variável com o
+	identificador "y". O tipo desta variável deve ser o tipo subjacente do tipo que você
+	criou no exercício anterior.
+    2. Na função main:
+        1. Isto já deve estar feito:
+            1. Demonstre o valor da variável "x"
+            2. Demonstre o tipo da variável "x"
+            3. Atribua 42 à variável "x" utilizando o operador "="
+            4. Demonstre o valor da variável "x"
+        2. Agora faça tambem:
+            1. Utilize conversão para transformar o tipo do valor da variável "x" em seu
+			tipo subjacente e, utilizando o operador "=", atribua o valor de "x" a "y"
+            2. Demonstre o valor de "y"
+            3. Demonstre o tipo de "y"
+*/
+type N1 int
+
+var x N1
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", y)
+	x = 42
+	fmt.Println(x)
+
+	y = int(x)
+	fmt.Println(y)
+
+}
