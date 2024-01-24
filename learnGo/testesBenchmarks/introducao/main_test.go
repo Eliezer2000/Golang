ba18876591b21e4cc706b051747a6ddc8ccf148d
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2024-01-23 21:41:57 -0300

    feat: learnGo testes e benchmarks: testes em tabela

--- learnGo/testesBenchmarks/introducao/main_test.go
+++ learnGo/testesBenchmarks/introducao/main_test.go
@@ -2,6 +2,26 @@ package main
 
 import "testing"
 
+
+type test struct{
+	data []int
+	answer int
+}
+
+func TestSomaEmTabela(t *testing.T ){
+	tests := []test {
+		test{data: []int{1, 2, 3}, answer: 6},
+		test{[]int{10, 11, 12}, 33}, 
+		test{[]int{-5, 0, 5, 10}, 10},
+	}
+	for _, v := range tests {
+		z := soma(v.data...)
+		if z != v.answer {
+			t.Error("Expected: ", v.answer, "Got: ", z)
+		} 
+	}
+}
+
 func TestSoma(t *testing.T){
 	test := soma(1, 2, 3)
 	resul := 6
