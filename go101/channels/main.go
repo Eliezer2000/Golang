af8c3238d141815211d9ecba3dfaa1469bd4f35a
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2024-01-27 12:50:18 -0300

    feat: GO 101: Channels

---
+++ go101/channels/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros(done chan<- bool) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", i)
+		time.Sleep(time.Millisecond * 150)
+	}
+	done <- true
+}
+
+func letras(done chan<- bool) {
+	for l := 'a'; l < 'j'; l++{
+		fmt.Printf("%c ", l)
+		time.Sleep(time.Millisecond * 230)
+	} 
+	done <- true
+}
+
+func main(){
+	cn := make(chan bool)
+	cl := make(chan bool)
+	go numeros(cn)
+	go letras(cl)
+	time.Sleep(3 * time.Second)
+	<- cn 
+	<- cl
+	fmt.Println("Fim da execução")
+}
