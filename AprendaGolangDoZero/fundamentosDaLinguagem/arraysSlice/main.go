c12d7f08bcb9b6da8cb3b081e76e8439ca6ea898
Eliezer Alves <eliezeralvesbernardino@gmail.com>
2024-01-28 21:17:14 -0300

    feat: Aprenda golang do zero: Fundamentos da linguagem: arrays and Slice

---
+++ AprendaGolangDoZero/fundamentosDaLinguagem/arraysSlice/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(slice)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+
+	slice = append(slice, 19)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição alterada"
+	fmt.Println(slice2)
+}
